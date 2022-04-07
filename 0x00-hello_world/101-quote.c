243ca8efa20baae8c9a26c10411e6edbd54cdc3f
Temitope Shobayo <shobayomykel@gmail.com>
2022-04-07 12:00:57 -0700

    Advanced task 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * print and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: 0
+ */
+int main(void)
+{
+
+	quote("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+
+}
