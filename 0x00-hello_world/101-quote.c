a5dab89b6548931d184358a5fd75f8a7f2bca3ae
Temitope Shobayo <shobayomykel@gmail.com>
2022-04-07 12:04:47 -0700

    Advanced task 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - print the string in the put function
  *
  * Description: using the main function
- * print and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * print and that piece of art is useful\" - Dora Korpar, 2015-10-19
  * Return: 0
  */
 int main(void)
