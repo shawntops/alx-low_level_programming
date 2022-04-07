a1e557a3b1c7d706eeae17bdcc9e126dcb76db45
Temitope Shobayo <shobayomykel@gmail.com>
2022-04-07 10:59:10 -0700

    Task 4 ALX

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
