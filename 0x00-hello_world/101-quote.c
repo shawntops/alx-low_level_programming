69de5bf6d8db92840a93d7cca7cb24d764d19190
Temitope Shobayo <shobayomykel@gmail.com>
2022-04-07 12:09:06 -0700

    Advanced task 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 
-	quote("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	quote("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
 	return (1);
 
 }
