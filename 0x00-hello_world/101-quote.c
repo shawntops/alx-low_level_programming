25dc72594ed9152b61534b6888d7a2abc66da89e
Temitope Shobayo <shobayomykel@gmail.com>
2022-04-07 12:21:07 -0700

    Advanced task 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - print the string in the put function
@@ -10,7 +11,7 @@
 int main(void)
 {
 
-	quote(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
