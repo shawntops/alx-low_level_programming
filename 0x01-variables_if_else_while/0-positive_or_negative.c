63122183fd56dcb1a36190bdefdfc9b7bfd36266
Temitope Shobayo <shobayomykel@gmail.com>
2022-04-08 09:49:26 -0700

    Variable if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,15 +7,10 @@
 
 
 /**
- *
  * main - print if the number is postive, zero, or negative
  *
- *
- *
  * Description: using the main function
- *
  * this program prints "Programming is positive, zero, or negative
- *
  * Return: 0
  *
  */
