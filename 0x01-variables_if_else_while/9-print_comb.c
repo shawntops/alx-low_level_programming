fab5817684614016499921635dd3e92af8607d5a
Temitope Shobayo <shobayomykel@gmail.com>
2022-04-10 05:55:36 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,25 +16,21 @@ int main(void)
 
 
 
-			for (c = 48; c <= 57; c++)
+		for (c = 48; c <= 57; c++)
 
 		{
+					putchar(c);
 
-								putchar(c);
-
-							if (c != 57)
-
-						{
-
+					if (c != 57)
+					{
 							putchar(',');
 
 							putchar(' ');
-
-						}
+					}
 		}
 
-				putchar('\n');
+		putchar('\n');
 
-					return (0);
+		return (0);
 
 }
