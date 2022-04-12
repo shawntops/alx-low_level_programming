fc8ff49dbc3d2a575d2b035c2b200c19af6a56b6
Temitope Shobayo <shobayomykel@gmail.com>
2022-04-12 08:23:40 -0700

    _putchar

---
+++ 0-putchar.c
@@ -0,0 +1,18 @@
+#inclue <stdio.h>
+
+/**
+ * main -program that prints _putchar
+ *
+ * Description : using the main function
+ * the program prints _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char " _putchar"
+	{
+	       	_putchar ( "_putchar")
+	}
+_putchar('\n');
+return (0);
+}
